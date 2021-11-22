d21c29184c374b6471793645017d9d6fb0a685f0
Benjamin Masi <benjamin.masi45@gmail.com>
2021-11-22 16:40:44 +0100

    :construction: - basic test methods signature added in sendinblueClient

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,4 +4,20 @@ namespace App\Entity;
 
 class User
 {
+    /**
+     * Id in sendmail
+     *
+     * @var string
+     */
+    private $token;
+
+    public function setToken(string $token)
+    {
+        $this->token = $token;
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
 }
--- src/Service/SendinBlueClient.php
+++ src/Service/SendinBlueClient.php
@@ -2,7 +2,9 @@
 
 namespace App\Service;
 
+use App\Entity\User;
 use SendinBlue;
+use Sendinblue\Client\ApiException;
 
 class SendinBlueClient
 {
@@ -10,5 +12,41 @@ class SendinBlueClient
     {
         $this->config = SendinBlue\Client\Configuration::getDefaultConfiguration()
             ->setApiKey('api-key', $sendinblueApiKey);
+    }
+
+    /**
+     * Retreive a contact based on its token
+     *
+     * @param string $token
+     *
+     * @throws Sendinblue\ApiException
+     *
+     * @return User|null
+     *
+     */
+    public function getContact(string $token) : ?User
+    {
+        return null;
+    }
+
+
+    /**
+     * Update the given contact in sendinblue contact database
+     *
+     * @param User $user
+     *
+     * @return User
+     */
+    public function updateContact(User $user) : User
+    {
+
+        return $user;
+    }
+
+    private function mapAttributesToUser(array $attributes) : User
+    {
+        $user = new User();
+
+        return $user;
     }
 }
