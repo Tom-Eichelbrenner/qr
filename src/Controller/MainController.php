71132fafdca5896d41af81e13885172fbb07522d
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-11-23 10:12:59 +0100

    ✨ creation of routes and associated twig templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,4 +18,72 @@ class MainController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    /**
+     * @Route("/je-participe/{token}", name="participation", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function participation($token)
+    {
+        return $this->render('je-participe.html.twig', [
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * @Route("/je-ne-participe-pas", name="desistement", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function jeNeParticipePas()
+    {
+        return $this->render('je-ne-participe-pas.html.twig');
+    }
+
+    /**
+     * @Route("/je-participe/2/{token}", name="formulaire", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function formulaire($token)
+    {
+        return $this->render('formulaire.html.twig', [
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * @Route("/règles-sanitaires", name="règles-sanitaires", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function reglesSanitaires()
+    {
+        return $this->render('règles-sanitaires.html.twig');
+    }
+
+    /**
+     * @Route("/je-participe/confirmation/{token}", name="confirmation", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function confirmation($token)
+    {
+        return $this->render('confirmation.html.twig', [
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * @Route("/inscrit/{id}", name="inscrit", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function inscrit($id)
+    {
+        return $this->render('inscrit.html.twig', [
+            'id' => $id
+        ]);
+    }
 }
