3e3f5655e4c3cc75c3dd2d44ba6a6734ca59f730
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-11-26 14:04:19 +0100

    ✨ Migrating all labels into translator yaml file.

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -15,9 +15,11 @@ use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserType extends AbstractType
 {
+    private $translator;
 
     public const STEP1 = 1;
     public const STEP2 = 2;
@@ -26,9 +28,21 @@ class UserType extends AbstractType
      */
     private $security;
 
-    public function __construct(Security $security)
+
+    public function __construct(Security $security, TranslatorInterface $translator)
     {
         $this->security = $security;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param $message
+     *
+     * @return string
+     */
+    public function getTranslation($message): string
+    {
+        return $this->translator->trans($message);
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -39,31 +53,31 @@ class UserType extends AbstractType
                     'choices' => array_flip(User::CIVILITY),
                     'expanded' => false,
                     'multiple' => false,
-                    'label' => 'Civilité *',
+                    'label' => $this->getTranslation('form_part_1.civilite.label'),
                 ])
                 ->add('lastName', null, [
-                    'label' => 'Nom *',
+                    'label' => $this->getTranslation('form_part_1.nom.label'),
                 ])
                 ->add('firstName', null, [
-                    'label' => 'Prénom *',
+                    'label' => $this->getTranslation('form_part_1.prenom.label'),
                 ])
                 ->add('email', null, [
-                    'label' => 'Email *',
+                    'label' => $this->getTranslation('form_part_1.email.label'),
                     'disabled' => true,
                 ])
                 ->add('phone', TelType::class, [
-                    'label' => 'Téléphone mobile *',
-                    'help' => 'Ce numéro sera utilisé uniquement pour vous envoyer des informations sur l\'événement en cas de besoin.',
+                    'label' => $this->getTranslation('form_part_1.phone.label'),
+                    'help' => $this->getTranslation('form_part_1.phone.help'),
                 ])
                 ->add('imageRight', CheckboxType::class, [
-                    'label' => 'Droit a l\'image',
+                    'label' => $this->getTranslation('form_part_1.image_right.label'),
                     'attr' => [
                         'class' => 'switch-custom',
                     ],
                     'required' => true,
                 ])
                 ->add('submit', SubmitType::class, [
-                    'label' => 'Etape suivante',
+                    'label' => $this->getTranslation('form_part_1.submit.label'),
                 ]);
         }
         // get user data
@@ -72,8 +86,8 @@ class UserType extends AbstractType
         if ($options['step'] === self::STEP2) {
             $builder
                 ->add('hotel', CheckboxType::class, [
-                    'label' => 'Je confirme souhaiter bénéficier de cette réservation hôtelière.',
-                    'help' => "Les coordonnées et accès vous seront précisés dans le mail de confirmation",
+                    'label' => $this->getTranslation('form_part_2.hotel.label'),
+                    'help' => $this->getTranslation('form_part_2.hotel.help'),
                     'required' => false,
                     'attr' => [
                         'class' => 'switch-custom',
@@ -83,7 +97,7 @@ class UserType extends AbstractType
                 if ($user->getHotelName() === "1") {
                     $builder
                         ->add('transfertPleniereWestin', CheckboxType::class, [
-                            'label' => 'Transfert de la plénière au Westin',
+                            'label' => $this->getTranslation('form_part_2.transfert.pleniere_westin.label'),
                             'required' => false,
                             'attr' => [
                                 'class' => 'switch-custom',
@@ -92,14 +106,14 @@ class UserType extends AbstractType
                     if ($user->getDinner()) {
                         $builder
                             ->add('transfertWestinDinner', CheckboxType::class, [
-                                'label' => 'Transfert du Westin au restaurant',
+                                'label' => $this->getTranslation('form_part_2.transfert.westin_dinner.label'),
                                 'required' => false,
                                 'attr' => [
                                     'class' => 'switch-custom',
                                 ],
                             ])
                             ->add('transfertDinnerWestin', CheckboxType::class, [
-                                'label' => 'Transfert du repas au Westin',
+                                'label' => $this->getTranslation('form_part_2.transfert.dinner_westin.label'),
                                 'required' => false,
                                 'attr' => [
                                     'class' => 'switch-custom',
@@ -110,7 +124,7 @@ class UserType extends AbstractType
                 if ($user->getHotelName() === "2") {
                     $builder
                         ->add('transfertPleniereInter', CheckboxType::class, [
-                            'label' => 'Transfert de la plénière au Inter',
+                            'label' => $this->getTranslation('form_part_2.transfert.pleniere_inter.label'),
                             'required' => false,
                             'attr' => [
                                 'class' => 'switch-custom',
@@ -119,14 +133,14 @@ class UserType extends AbstractType
                     if ($user->getDinner()) {
                         $builder
                             ->add('transfertInterDinner', CheckboxType::class, [
-                                'label' => 'Transfert du Inter au restaurant',
+                                'label' => $this->getTranslation('form_part_2.transfert.inter_dinner.label'),
                                 'required' => false,
                                 'attr' => [
                                     'class' => 'switch-custom',
                                 ],
                             ])
                             ->add('transfertDinnerInter', CheckboxType::class, [
-                                'label' => 'Transfert du repas au Inter',
+                                'label' => $this->getTranslation('form_part_2.transfert.dinner_inter.label'),
                                 'required' => false,
                                 'attr' => [
                                     'class' => 'switch-custom',
@@ -138,7 +152,7 @@ class UserType extends AbstractType
             if (!$user->getHotel() && $user->getDinner()) {
                 $builder
                     ->add('transfertTaxi', CheckboxType::class, [
-                        'label' => 'Transfert en taxi',
+                        'label' => $this->getTranslation('form_part_2.transfert.taxi.label'),
                         'required' => false,
                         'attr' => [
                             'class' => 'switch-custom',
@@ -147,14 +161,14 @@ class UserType extends AbstractType
                 if ($user->getTransfertTaxi()) {
                     $builder
                         ->add('taxiAdress', null, [
-                            'label' => 'Adresse pour le taxi',
+                            'label' => $this->getTranslation('form_part_2.transfert.taxi_adress.label'),
                         ]);
                 }
             }
             if ($user->getDinner()) {
                 $builder
                     ->add('dinner', CheckboxType::class, [
-                        'label' => 'J\'ai un régime ou une intolérance alimentaire spécifique',
+                        'label' => $this->getTranslation('form_part_2.dinner.label'),
                         'required' => false,
                         'attr' => [
                             'class' => 'switch-custom',
@@ -162,12 +176,12 @@ class UserType extends AbstractType
                     ])
                     ->add('diet', null, [
                         'required' => false,
-                        'label' => 'Si oui, veuillez nous le préciser',
+                        'label' => $this->getTranslation('form_part_2.diet.label'),
                     ]);
             }
             $builder
                 ->add('submit', SubmitType::class, [
-                    'label' => 'Valider mon inscription',
+                    'label' => $this->getTranslation('form_part_2.submit.label'),
                 ]);
         }
     }
