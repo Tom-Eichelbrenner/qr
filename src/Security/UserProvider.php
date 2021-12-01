4b19a3b64eda8313068b204d2e2354a3faff750b
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-12-01 10:43:08 +0100

    ♻️ refactoring UserProvider

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -4,13 +4,12 @@ namespace App\Security;
 
 use App\Entity\User;
 use App\Service\SendinBlueClient;
+use Sendinblue\Client\ApiException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-/**
- * @method UserInterface loadUserByIdentifier(string $identifier)
- */
+
 class UserProvider implements UserProviderInterface
 {
     private $sendinblueClient;
@@ -19,6 +18,7 @@ class UserProvider implements UserProviderInterface
     {
         $this->sendinblueClient = $sendinblueClient;
     }
+
     /**
      * @param UserInterface $user
      *
@@ -39,19 +39,15 @@ class UserProvider implements UserProviderInterface
         return User::class === $class;
     }
 
-    /**
-     * @param string $username
-     *
-     * @return UserInterface
-     */
-    public function loadUserByUsername(string $username): UserInterface
-    {
-    }
 
     public function __call($name, $arguments)
     {
         // TODO: Implement @method UserInterface loadUserByIdentifier(string $identifier)
     }
+
+    /**
+     * @throws ApiException
+     */
     public function loadUserByIdentifier(string $identifier): UserInterface
     {
         $user = $this->sendinblueClient->getContact($identifier);
@@ -59,5 +55,17 @@ class UserProvider implements UserProviderInterface
             return $user;
         }
         throw new UserNotFoundException("User with identifier $identifier doesnt exist");
+    }
+
+    /**
+     * @param string $username
+     *
+     * @return UserInterface
+     * @throws ApiException
+     */
+    public function loadUserByUsername(string $username): UserInterface
+    {
+        return $this->loadUserByIdentifier($username);
+        // TODO: Implement loadUserByUsername() method.
     }
 }
