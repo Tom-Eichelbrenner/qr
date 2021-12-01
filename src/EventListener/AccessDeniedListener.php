0e7b25e60de66060fb869cb1db2f2381c39e2bbe
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-12-01 10:05:46 +0100

    :sparkles: Verification that the user has the right to access this route

---
+++ src/EventListener/AccessDeniedListener.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class AccessDeniedListener implements EventSubscriberInterface
+{
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+
+    public function __construct(RouterInterface $router, SessionInterface $session)
+    {
+        $this->router = $router;
+        $this->session = $session;
+    }
+
+    /**
+     * @return \array[][]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => [
+                ['onKernelException', 999999],
+            ],
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $message = $event->getThrowable()->getMessage();
+        if ($exception instanceof AccessDeniedException || $exception instanceof NotFoundHttpException || $exception instanceof UnauthorizedHttpException || $exception instanceof HttpException) {
+            $url = $this->router->generate('error');
+            $response = new RedirectResponse($url);
+            $response->headers->set('Location', $url);
+            // get the session
+            $session = $this->session;
+            $session->set('message', $message);
+            $event->setResponse($response);
+        }
+    }
+}
